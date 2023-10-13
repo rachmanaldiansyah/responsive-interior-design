7d9a47fd0549d321188753bd54a1470a3776bbce
Rachman Aldiansyah <rachman@pop-os.localdomain>
2023-10-13 14:29:02 +0700

    create reference section

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Hero />
       <Services />
       <Products />
-      {/* <Reference /> */}
+      <Reference />
       {/* <Care /> */}
       {/* <Footer /> */}
     </>
